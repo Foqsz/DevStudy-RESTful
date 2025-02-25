ff975433026d3207c3e65f250c3f43dc8a4f52d0
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-25 09:54:00 -0300

    feat: data base

---
+++ DevStudy.Infrastructure/Data/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using DevStudy.Core.Models;
+using Microsoft.EntityFrameworkCore; 
+
+namespace DevStudy.Infrastructure.Data;
+
+public class DataBaseContext : DbContext
+{
+    public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+    {
+    }
+    public DbSet<Aluno> Alunos { get; set; }
+    public DbSet<Exercicio> Exercicios { get; set; }
+    public DbSet<Treino> Treinos { get; set; }
+    public DbSet<TreinoExercicio> TreinoExercicios { get; set; }
+    public DbSet<Plano> Planos { get; set; }
+    public DbSet<Pagamento> Pagamentos { get; set; }
+    public DbSet<Instrutor> Instrutores { get; set; }
+    public DbSet<AvaliacaoFisica> AvaliacoesFisicas { get; set; }
+}
