2c79b03ec9112a093ad79814e90e008d4bfe0fa3
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-26 11:40:46 -0300

    feat: DTO para update

---
+++ DevStudy.Application/DTOs/AlunoUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Application.DTOs;
+
+public class AlunoUpdateDTO
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Plano { get; set; }
+    public bool Ativo { get; set; }
+}
