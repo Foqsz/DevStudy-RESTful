292eb7c10fb8bf3b36128ded0c06c7185d6f42b8
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-26 11:41:45 -0300

    feat: adicionando o update dto no mapping

--- DevStudy.Application/Mappers/Mapping/AlunoMappingProfile.cs
+++ DevStudy.Application/Mappers/Mapping/AlunoMappingProfile.cs
@@ -15,5 +15,6 @@ public class AlunoMappingProfile : Profile
     {
         CreateMap<AlunoDTO, Aluno>().ReverseMap();
         CreateMap<AlunoCreateDTO, Aluno>().ReverseMap();
+        CreateMap<AlunoUpdateDTO, Aluno>().ReverseMap();
     } 
 }
