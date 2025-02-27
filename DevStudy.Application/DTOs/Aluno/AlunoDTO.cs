f69d33fac3c52fbb407163cc0beca005f8569885
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-27 10:26:46 -0300

    feat: nova pasta pra alunos em DTO. manter organizado.

---
+++ DevStudy.Application/DTOs/Aluno/AlunoDTO.cs
@@ -0,0 +1,18 @@
+﻿using DevStudy.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Application.DTOs.Aluno;
+
+public class AlunoDTO
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Plano { get; set; }
+    public bool Ativo { get; set; }
+    public List<Treino> Treinos { get; set; } // Relacionamento com Treinos
+}
