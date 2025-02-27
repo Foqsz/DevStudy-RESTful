aed2d78be3a6b9c9581f2007322315f54bd91107
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-27 10:27:05 -0300

    feat: nova pasta para treino. E realocando a de alunos.

---
+++ DevStudy.Application/DTOs/Treino/TreinoCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Application.DTOs.Treino;
+
+public class TreinoCreateDTO
+{
+    public int AlunoId { get; set; }
+    public DateTime Data { get; set; }
+}
---
+++ DevStudy.Application/DTOs/Treino/TreinoDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Application.DTOs.Treino;
+
+public class TreinoDTO
+{
+    public int Id { get; set; }
+    public DateTime Data { get; set; }
+    public List<ExerciciosDTO> Exercicios { get; set; } // Lista de exercícios do treino 
+
+}
