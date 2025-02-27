a781f4ef4a8db79a30504748520499478e54b4cf
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-27 11:18:03 -0300

    feat: controller dos treinos

---
+++ DevStudy.API/Controller/TreinosController.cs
@@ -0,0 +1,82 @@
+﻿using DevStudy.Application.DTOs.Treino;
+using DevStudy.Application.Interfaces;
+using DevStudy.Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevStudy.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreinosController : ControllerBase
+    {
+        private readonly ITreinosService _treinosService;
+        private ILogger<TreinosController> _logger;
+
+        public TreinosController(ITreinosService treinosService, ILogger<TreinosController> logger)
+        {
+            _treinosService = treinosService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TreinoDTO>>> GetTreinos()
+        {
+            try
+            {
+                var treinosAll = await _treinosService.GetTreinos();
+                if (!treinosAll.Any())
+                {
+                    _logger.LogError("Nenhum treino encontrado");
+                    return NotFound();
+                }
+                return Ok(treinosAll);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro ao buscar treinos");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao buscar treinos");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TreinoDTO>> GetTreinoById(int id)
+        {
+            try
+            {
+                var treinoId = await _treinosService.GetTreinoById(id);
+                if (treinoId == null)
+                {
+                    _logger.LogError("Treino não encontrado");
+                    return NotFound();
+                }
+                return Ok(treinoId);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro ao buscar treino");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao buscar treino");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TreinoCreateDTO>> CreateTreino(TreinoCreateDTO treino)
+        {
+            try
+            {
+                var newTreino = await _treinosService.CreateTreino(treino);
+                if (newTreino == null)
+                {
+                    _logger.LogError("Treino não criado");
+                    return NotFound();
+                }
+                return Ok(newTreino);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro ao criar treino");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao criar treino");
+            }
+        }
+    }
+}
