409d74ac4354232ccd453659ef6d0d73ef5528e8
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-28 11:52:42 -0300

    feat: Dto criado pra o metodo Create dos treino exercicios

---
+++ DevStudy.Application/DTOs/TreinoExercicio/TreinoExercicioCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Application.DTOs.TreinoExercicio;
+
+public class TreinoExercicioCreateDTO
+{
+    public int Id { get; set; }
+
+    public int TreinoId { get; set; }
+
+    public int ExercicioId { get; set; }
+
+    public int Repeticoes { get; set; }
+
+    public int Series { get; set; }
+}
