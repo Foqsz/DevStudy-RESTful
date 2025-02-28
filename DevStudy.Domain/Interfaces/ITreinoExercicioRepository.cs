d30c99b49904f4c3a5ec785eea27e86948af6f5a
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-28 11:55:45 -0300

    feat: interface do treinoRepositorio

---
+++ DevStudy.Domain/Interfaces/ITreinoExercicioRepository.cs
@@ -0,0 +1,17 @@
+﻿using DevStudy.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Domain.Interfaces;
+
+public interface ITreinoExercicioRepository
+{
+    Task<IEnumerable<TreinoExercicio>> GetTreinoExercicios();
+    Task<TreinoExercicio> GetTreinoExercicioById(int id);
+    Task<TreinoExercicio> CreateTreinoExercicio(TreinoExercicio treinoExercicio);
+    Task<TreinoExercicio> UpdateTreinoExercicio(int id, TreinoExercicio treinoExercicio);
+    Task<bool> DeleteTreinoExercicio(int id);
+}
