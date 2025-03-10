81a749d7059c7ae482760b987352c01e50bc8036
Foqsz <contatovictorvinicius05@gmail.com>
2025-03-10 11:37:02 -0300

    refactor: faltou adicionar Id no Dto.

---
+++ DevStudy.Application/DTOs/Instrutor/InstrutorDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Application.DTOs.Instrutor
+{
+    public class InstrutorDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Especialidade { get; set; }  
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string Telefone { get; set; }
+    }
+}
