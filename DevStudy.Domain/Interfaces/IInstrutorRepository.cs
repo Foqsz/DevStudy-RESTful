52fc871e3b0c7261580d3d533ef7d945039743eb
Foqsz <contatovictorvinicius05@gmail.com>
2025-03-10 10:20:11 -0300

    feat: service e repositorio do Instrutor

---
+++ DevStudy.Domain/Interfaces/IInstrutorRepository.cs
@@ -0,0 +1,18 @@
+﻿using DevStudy.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Domain.Interfaces;
+
+public interface IInstrutorRepository
+{
+    Task<IEnumerable<Instrutor>> GetInstrutores();
+    Task<Instrutor> GetInstrutor(int id);
+    Task<Instrutor> GetInstrutorByEmail(string email);
+    Task<Instrutor> CreateInstrutor(Instrutor instrutor);
+    Task<Instrutor> UpdateInstrutor(int id, Instrutor instrutor);
+    Task<bool> DeleteInstrutor(int id);
+}
---
+++ DevStudy.Infrastructure/Repository/InstrutorRepository.cs
@@ -0,0 +1,108 @@
+﻿
+using DevStudy.Domain.Interfaces;
+using DevStudy.Domain.Models;
+using DevStudy.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Infrastructure.Repository;
+
+public class InstrutorRepository : IInstrutorRepository
+{
+    private readonly DataBaseContext _context;
+    private ILogger<InstrutorRepository> _logger;
+
+    public InstrutorRepository(DataBaseContext context, ILogger<InstrutorRepository> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task<IEnumerable<Instrutor>> GetInstrutores()
+    {
+        return await _context.Instrutores.Include(a => a.Alunos).ToListAsync();
+    }
+
+    public async Task<Instrutor> GetInstrutor(int id)
+    {
+        return await _context.Instrutores.FindAsync(id);
+    }
+
+    public async Task<Instrutor> GetInstrutorByEmail(string email)
+    {
+        return await _context.Instrutores.SingleOrDefaultAsync(a => a.Email == email);
+    }
+
+    public async Task<Instrutor> CreateInstrutor(Instrutor instrutor)
+    {
+        var emailExist = await _context.Instrutores.AnyAsync(a => a.Email == instrutor.Email);
+
+        if (emailExist)
+        {
+            _logger.LogError("Email já cadastrado");
+            return null;
+        }
+
+        var phoneExist = await _context.Instrutores.AnyAsync(i => i.Telefone == instrutor.Telefone);
+
+        if (phoneExist)
+        {
+            _logger.LogError("Telefone já cadastrado");
+            return null;
+        }
+
+        _context.Instrutores.Add(instrutor);
+        await _context.SaveChangesAsync();
+        return instrutor;
+    }
+
+    public async Task<Instrutor> UpdateInstrutor(int id, Instrutor instrutor)
+    {
+        var instrutorExist = await _context.Instrutores.AnyAsync(i => i.Id == id);
+
+        if (!instrutorExist)
+        {
+            _logger.LogError("Instrutor não encontrado");
+            return null;
+        }
+
+        var emailExist = await _context.Instrutores.AnyAsync(i => i.Email == instrutor.Email && i.Id != id);
+        if (emailExist)
+        {
+            _logger.LogError("Email já cadastrado");
+            return null;
+        }
+
+        var phoneExist = await _context.Instrutores.AnyAsync(i => i.Telefone == instrutor.Telefone && i.Id != id);
+        if (phoneExist)
+        {
+            _logger.LogError("Telefone já cadastrado");
+            return null;
+        }
+
+        _context.Instrutores.Update(instrutor);
+        await _context.SaveChangesAsync();
+        return instrutor;
+    }
+
+    public async Task<bool> DeleteInstrutor(int id)
+    {
+        var deleteInstrutor = await _context.Instrutores.FindAsync(id);
+
+        if (deleteInstrutor == null)
+        {
+            _logger.LogError("Instrutor não encontrado");
+            return false;
+        }
+
+        _context.Instrutores.Remove(deleteInstrutor);
+        _context.SaveChanges();
+        return true;
+    }
+}
