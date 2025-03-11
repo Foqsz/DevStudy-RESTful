87b2ebfbb09aa5680a5aeec16c4602227a229a89
Foqsz <contatovictorvinicius05@gmail.com>
2025-03-11 12:10:23 -0300

    feat: atualizaçao na classe plano

--- DevStudy.Domain/Models/Plano.cs
+++ DevStudy.Domain/Models/Plano.cs
@@ -12,5 +12,6 @@ public class Plano
     public string Nome { get; set; } // Ex: "Mensal", "Trimestral"
     public decimal Preco { get; set; }
     public string Descricao { get; set; }
-    public int DuracaoMeses { get; set; } // Duração do plano em meses
+    public DateTime DataInicio { get; set; } // Data de início do plano
+    public DateTime DataFim { get; set; } // Data de término do plano
 }
