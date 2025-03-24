cfc7d79caeed9479177cdc188b6a8f2f969cc82a
Foqsz <contatovictorvinicius05@gmail.com>
2025-03-24 10:16:39 -0300

    feat: DTO para Avaliacao fisica. Evitar listagem de dados desnecessarios

---
+++ DevStudy.Application/DTOs/AvaliaoFisica/AvaliacaoFisicaDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Application.DTOs.AvaliacaoFisica;
+
+public class AvaliacaoFisicaDTO
+{
+
+    public int Id { get; set; }
+
+    public int AlunoId { get; set; } // Relacionamento com Aluno
+
+    public DateTime Data { get; set; }
+
+    public decimal Peso { get; set; }
+
+    public decimal Altura { get; set; } 
+}
---
+++ DevStudy.Application/Mappers/Mapping/AvaliacaoFisicaMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DevStudy.Application.DTOs.AvaliacaoFisica;
+using DevStudy.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStudy.Application.Mappers.Mapping;
+
+public class AvaliacaoFisicaMappingProfile : Profile
+{
+    public AvaliacaoFisicaMappingProfile()
+    {
+        CreateMap<AvaliacaoFisicaDTO, AvaliacaoFisica>().ReverseMap();
+    }
+}
