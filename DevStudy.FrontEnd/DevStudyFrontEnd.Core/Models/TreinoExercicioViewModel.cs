c324711ab4f000aeb41c96b90da3c119016dfefe
Foqsz <contatovictorvinicius05@gmail.com>
2025-03-28 12:20:36 -0300

    refactor: ajuste nas view models

--- DevStudy.FrontEnd/DevStudyFrontEnd.Core/Models/TreinoExercicioViewModel.cs
+++ DevStudy.FrontEnd/DevStudyFrontEnd.Core/Models/TreinoExercicioViewModel.cs
@@ -11,6 +11,7 @@ public class TreinoExercicioViewModel
 
     [Required]
     public int ExercicioId { get; set; } // Relacionamento com Exercicio
+    public int AlunoId { get; set; } // Chave estrangeira para Aluno
 
     [Range(1, int.MaxValue, ErrorMessage = "Repeticoes must be greater than 0")]
     public int Repeticoes { get; set; } // Quantidade de repetições do exercício
@@ -18,4 +19,5 @@ public class TreinoExercicioViewModel
     [Range(1, int.MaxValue, ErrorMessage = "Series must be greater than 0")]
     public int Series { get; set; } // Quantidade de séries
     public ExercicioViewModel Exercicio { get; set; }
+    public AlunoViewModel Aluno { get; set; }
 }
--- DevStudy.FrontEnd/DevStudyFrontEnd.Core/Models/TreinoViewModel.cs
+++ DevStudy.FrontEnd/DevStudyFrontEnd.Core/Models/TreinoViewModel.cs
@@ -5,6 +5,7 @@ namespace DevStudy.FrontEnd.DevStudyFrontEnd.Core.Models;
 public class TreinoViewModel
 {
     public int Id { get; set; }
+    public int AlunoId { get; set; }
     public DateTime Data { get; set; }
-    public List<ExercicioViewModel> Exercicios { get; set; } // Lista de exercícios do treino 
+    public int ExercicioId { get; set; } = new();
 }
