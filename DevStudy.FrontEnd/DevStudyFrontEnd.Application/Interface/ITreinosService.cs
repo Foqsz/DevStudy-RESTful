9f81717ac0df3de598ab76693a522e52b6e2a994
Foqsz <contatovictorvinicius05@gmail.com>
2025-03-28 12:20:18 -0300

    feat: service e interface dos treinos

---
+++ DevStudy.FrontEnd/DevStudyFrontEnd.Application/Interface/ITreinosService.cs
@@ -0,0 +1,12 @@
+﻿using DevStudy.FrontEnd.DevStudyFrontEnd.Core.Models;
+
+namespace DevStudy.FrontEnd.DevStudyFrontEnd.Application.Interface;
+
+public interface ITreinosService
+{
+    Task<IEnumerable<TreinoViewModel>> GetTreinos();
+    Task<TreinoViewModel> GetTreinoById(int id);
+    Task<TreinoViewModel> CreateTreino(TreinoViewModel treino);
+    Task<TreinoViewModel> UpdateTreino(int id, TreinoViewModel treino);
+    Task<bool> DeleteTreino(int id);
+}
---
+++ DevStudy.FrontEnd/DevStudyFrontEnd.Application/Service/TreinosService.cs
@@ -0,0 +1,101 @@
+﻿using DevStudy.FrontEnd.DevStudyFrontEnd.Application.Interface;
+using DevStudy.FrontEnd.DevStudyFrontEnd.Core.Models;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json;
+
+namespace DevStudy.FrontEnd.DevStudyFrontEnd.Application.Service;
+
+public class TreinosService : ITreinosService
+{
+    private readonly HttpClient _httpClient;
+    private const string url = "https://localhost:7238/api/Treinos";
+    private readonly JsonSerializerOptions _serializerOptions;
+
+    public TreinosService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
+
+    public async Task<IEnumerable<TreinoViewModel>> GetTreinos()
+    {
+        var client = _httpClient;
+        var response = await client.GetAsync($"{url}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var treinos = JsonConvert.DeserializeObject<IEnumerable<TreinoViewModel>>(content);
+            if (treinos == null)
+            {
+                throw new HttpRequestException("Erro ao desserializar a lista de treinos.");
+            }
+            return treinos;
+        }
+        throw new HttpRequestException($"Erro ao buscar os treinos. {response.StatusCode}");
+    }
+
+    public async Task<TreinoViewModel> GetTreinoById(int id)
+    {
+        var client = _httpClient;
+        var response = await client.GetAsync($"{url}/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var treino = JsonConvert.DeserializeObject<TreinoViewModel>(content);
+            if (treino == null)
+            {
+                throw new HttpRequestException("Erro ao desserializar o treino.");
+            }
+            return treino;
+        }
+        throw new HttpRequestException($"Erro ao buscar o treino. {response.StatusCode}");
+    }
+
+    public async Task<TreinoViewModel> CreateTreino(TreinoViewModel treino)
+    { 
+        var client = _httpClient; 
+
+        var response = await client.PostAsJsonAsync($"{url}", treino);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var addTreino = JsonConvert.DeserializeObject<TreinoViewModel>(content);
+            if (addTreino == null)
+            {
+                throw new HttpRequestException("Erro ao desserializar o treino adicionado.");
+            }
+            return addTreino;
+        }
+        throw new HttpRequestException($"Erro ao adicionar o treino. {response.StatusCode}");
+    }
+
+    public async Task<TreinoViewModel> UpdateTreino(int id, TreinoViewModel treino)
+    {
+        var client = _httpClient;
+        var content = new StringContent(JsonConvert.SerializeObject(treino), Encoding.UTF8, "application/json");
+        var response = await client.PutAsync($"{url}/{id}", content);
+        if (response.IsSuccessStatusCode)
+        {
+            var updateTreino = JsonConvert.DeserializeObject<TreinoViewModel>(await response.Content.ReadAsStringAsync());
+            if (updateTreino == null)
+            {
+                throw new HttpRequestException("Erro ao desserializar o treino atualizado.");
+            }
+            return updateTreino;
+        }
+        throw new HttpRequestException($"Erro ao atualizar o treino. {response.StatusCode}");
+    }
+
+    public async Task<bool> DeleteTreino(int id)
+    {
+        var client = _httpClient;
+        var response = await client.DeleteAsync($"{url}/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            return true;
+        }
+        throw new HttpRequestException($"Erro ao deletar o treino. {response.StatusCode}");
+    }
+}
