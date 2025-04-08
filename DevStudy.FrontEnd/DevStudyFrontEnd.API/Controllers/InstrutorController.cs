3d1952ad8211d299b6780d53d8d8eefbba749a31
Foqsz <contatovictorvinicius05@gmail.com>
2025-04-08 10:44:26 -0300

    feat: controller do instrutor com update, create e delete

--- DevStudy.FrontEnd/DevStudyFrontEnd.API/Controllers/InstrutorController.cs
+++ DevStudy.FrontEnd/DevStudyFrontEnd.API/Controllers/InstrutorController.cs
@@ -1,6 +1,7 @@
 ﻿using DevStudy.FrontEnd.DevStudyFrontEnd.Application.Interface;
 using DevStudy.FrontEnd.DevStudyFrontEnd.Core.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DevStudy.FrontEnd.DevStudyFrontEnd.API.Controllers
 {
@@ -18,6 +19,61 @@ namespace DevStudy.FrontEnd.DevStudyFrontEnd.API.Controllers
         {
             var instrutores = await _professorService.GetProfessores();
             return View(instrutores);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<InstrutorViewModel>> CreateInstrutor()
+        {
+            ViewBag.Id = new SelectList(await _professorService.GetProfessores());
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<InstrutorViewModel>> CreateInstrutor(InstrutorViewModel instrutor)
+        {
+            var instrutorCriado = await _professorService.CreateProfessor(instrutor);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<InstrutorViewModel>> UpdateInstrutor(int id)
+        {
+            var instrutores = await _professorService.GetProfessorById(id);
+            return View(instrutores);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<InstrutorViewModel>> UpdateInstrutor(int id, InstrutorViewModel instrutor)
+        {
+            if (!ModelState.IsValid)
+            {
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+                return View(instrutor);
+            }
+
+            var instrutorAtualizado = await _professorService.UpdateProfessor(id, instrutor);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<InstrutorViewModel>> DeleteInstrutor(int id)
+        {
+            var instrutor = await _professorService.GetProfessorById(id);
+            return View(instrutor);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<InstrutorViewModel>> DeleteInstrutor(int id, InstrutorViewModel instrutor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(instrutor);
+            }
+            var instrutorDeletado = await _professorService.DeleteProfessor(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
