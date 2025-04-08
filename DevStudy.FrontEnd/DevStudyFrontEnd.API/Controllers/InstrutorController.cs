168acbdc264090a36548e7dfefaec5de2a3b4413
Foqsz <contatovictorvinicius05@gmail.com>
2025-04-08 09:41:17 -0300

    git: controller do instrutor(professor).

---
+++ DevStudy.FrontEnd/DevStudyFrontEnd.API/Controllers/InstrutorController.cs
@@ -0,0 +1,23 @@
+﻿using DevStudy.FrontEnd.DevStudyFrontEnd.Application.Interface;
+using DevStudy.FrontEnd.DevStudyFrontEnd.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevStudy.FrontEnd.DevStudyFrontEnd.API.Controllers
+{
+    public class InstrutorController : Controller
+    {
+        private readonly IProfessorService _professorService;
+
+        public InstrutorController(IProfessorService professorService)
+        {
+            _professorService = professorService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InstrutorViewModel>>> Index()
+        {
+            var instrutores = await _professorService.GetProfessores();
+            return View(instrutores);
+        }
+    }
+}
