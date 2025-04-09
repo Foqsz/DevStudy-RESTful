bc94050516b5b1dde2581a2ed18ea55e9801a459
Foqsz <contatovictorvinicius05@gmail.com>
2025-04-09 11:35:24 -0300

    feat: controller avaliacao fisica

---
+++ DevStudy.FrontEnd/DevStudyFrontEnd.API/Controllers/AvaliacaoFisicaController.cs
@@ -0,0 +1,42 @@
+﻿using DevStudy.FrontEnd.DevStudyFrontEnd.Application.Interface;
+using DevStudy.FrontEnd.DevStudyFrontEnd.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DevStudy.FrontEnd.DevStudyFrontEnd.API.Controllers
+{
+    public class AvaliacaoFisicaController : Controller
+    {
+        private readonly IAvaliacaoFisicaService _avaliacaoFisicaService;
+
+        public AvaliacaoFisicaController(IAvaliacaoFisicaService avaliacaoFisicaService)
+        {
+            _avaliacaoFisicaService = avaliacaoFisicaService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AvaliacaoFisicaViewModel>>> Index()
+        {
+            var avaliacoes = await _avaliacaoFisicaService.GetAvaliacoesFisicas();
+            return View(avaliacoes); 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> CreateAvaliacaoFisica()
+        {
+            ViewBag.Id = new SelectList(await _avaliacaoFisicaService.GetAvaliacoesFisicas());
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAvaliacaoFisica(AvaliacaoFisicaViewModel avaliacaoFisica)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(avaliacaoFisica);
+            }
+            var avaliacaoCriada = await _avaliacaoFisicaService.CreateAvaliacaoFisica(avaliacaoFisica);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
