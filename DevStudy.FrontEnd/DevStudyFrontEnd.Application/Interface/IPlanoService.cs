20d586a5ffd63e6842256d4c752469f64c473f39
Foqsz <contatovictorvinicius05@gmail.com>
2025-04-10 12:15:17 -0300

    feat: service e interface dos planos

---
+++ DevStudy.FrontEnd/DevStudyFrontEnd.Application/Interface/IPlanoService.cs
@@ -0,0 +1,12 @@
+﻿using DevStudy.FrontEnd.DevStudyFrontEnd.Core.Models;
+
+namespace DevStudy.FrontEnd.DevStudyFrontEnd.Application.Interface;
+
+public interface IPlanoService
+{
+    Task<IEnumerable<PlanoViewModel>> GetPlanos();
+    Task<PlanoViewModel> GetPlano(int id);
+    Task<PlanoViewModel> CreatePlano(PlanoViewModel plano);
+    Task<PlanoViewModel> UpdatePlano(int id, PlanoViewModel plano);
+    Task<bool> DeletePlano(int id);
+}
---
+++ DevStudy.FrontEnd/DevStudyFrontEnd.Application/Service/PlanoService.cs
@@ -0,0 +1,107 @@
+﻿using DevStudy.FrontEnd.DevStudyFrontEnd.Application.Interface;
+using DevStudy.FrontEnd.DevStudyFrontEnd.Core.Models;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace DevStudy.FrontEnd.DevStudyFrontEnd.Application.Service;
+
+public class PlanoService : IPlanoService
+{
+    private readonly HttpClient _httpClient;
+    private const string url = "https://localhost:7238/api/Plano";
+    private readonly JsonSerializerOptions _serializerOptions;
+
+    public PlanoService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
+
+    public async Task<IEnumerable<PlanoViewModel>> GetPlanos()
+    {
+        var client = _httpClient;
+
+        var response = await client.GetAsync($"{url}");
+
+        if(response.IsSuccessStatusCode)
+        {
+            var content = response.Content.ReadAsStringAsync();
+            var planos = JsonConvert.DeserializeObject<IEnumerable<PlanoViewModel>>(content.Result);
+            if (planos == null)
+            {
+                throw new HttpRequestException("Erro ao desserializar a lista de planos.");
+            }
+            return planos;
+        }
+        throw new HttpRequestException($"Erro ao buscar os planos. {response.StatusCode}");
+    }
+
+    public async Task<PlanoViewModel> GetPlano(int id)
+    {
+        var client = _httpClient;
+        var response = client.GetAsync($"{url}/{id}");
+
+        if (response.Result.IsSuccessStatusCode)
+        {
+            var content = response.Result.Content.ReadAsStringAsync();
+            var plano = JsonConvert.DeserializeObject<PlanoViewModel>(content.Result);
+            if (plano == null)
+            {
+                throw new HttpRequestException("Erro ao desserializar o plano.");
+            }
+            return plano;
+        }
+        throw new HttpRequestException($"Erro ao buscar o plano. {response.Result.StatusCode}");
+    }  
+
+    public async Task<PlanoViewModel> CreatePlano(PlanoViewModel plano)
+    {
+        var client = _httpClient;
+        var response = await client.PostAsJsonAsync($"{url}", plano);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = response.Content.ReadAsStringAsync();
+            var planoCriado = JsonConvert.DeserializeObject<PlanoViewModel>(content.Result);
+            if (planoCriado == null)
+            {
+                throw new HttpRequestException("Erro ao desserializar o plano criado.");
+            }
+            return planoCriado;
+        }
+        throw new HttpRequestException($"Erro ao criar o plano. {response.StatusCode}");
+    }
+
+    public async Task<PlanoViewModel> UpdatePlano(int id, PlanoViewModel plano)
+    {
+        var client = _httpClient;
+
+        var response = await client.PutAsJsonAsync($"{url}/{id}", plano);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+
+            var planoAtualizado = JsonConvert.DeserializeObject<PlanoViewModel>(content);
+            if (planoAtualizado == null)
+            {
+                throw new HttpRequestException("Erro ao desserializar o plano atualizado.");
+            }
+            return planoAtualizado;
+        }
+        throw new HttpRequestException($"Erro ao atualizar o plano. {response.StatusCode}");
+    }
+
+    public async Task<bool> DeletePlano(int id)
+    {
+        var client = _httpClient;
+
+        var response = await client.DeleteAsync($"{url}/{id}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            return true;
+        } 
+        return false;
+    }   
+}
