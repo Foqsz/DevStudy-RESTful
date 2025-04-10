296226a02aaa7d275d53f6771d10b0561843cdea
Foqsz <contatovictorvinicius05@gmail.com>
2025-04-10 12:14:55 -0300

    feat: delete e update plano

---
+++ DevStudy.FrontEnd/DevStudyFrontEnd.API/Controllers/PlanoController.cs
@@ -0,0 +1,74 @@
+﻿using DevStudy.FrontEnd.DevStudyFrontEnd.Application.Interface;
+using DevStudy.FrontEnd.DevStudyFrontEnd.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DevStudy.FrontEnd.DevStudyFrontEnd.API.Controllers;
+
+public class PlanoController : Controller
+{
+    private readonly IPlanoService _planoService;
+
+    public PlanoController(IPlanoService planoService)
+    {
+        _planoService = planoService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PlanoViewModel>>> Index()
+    {
+        var planos = await _planoService.GetPlanos();
+        return View(planos);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PlanoViewModel>> CreatePlano()
+    {
+        ViewBag.Id = new SelectList(await _planoService.GetPlanos());
+ 
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PlanoViewModel>> CreatePlano(PlanoViewModel plano)
+    {
+        if (ModelState.IsValid)
+        {
+            await _planoService.CreatePlano(plano);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(plano);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PlanoViewModel>> UpdatePlano(int id)
+    {
+        var plano = await _planoService.GetPlano(id);
+        return View(plano);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PlanoViewModel>> UpdatePlano(int id, PlanoViewModel plano)
+    {
+        if (ModelState.IsValid)
+        {
+            await _planoService.UpdatePlano(id, plano);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(plano);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PlanoViewModel>> DeletePlano(int id)
+    {
+        var plano = await _planoService.GetPlano(id);
+        return View(plano);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PlanoViewModel>> DeletePlano(int id, PlanoViewModel plano)
+    {
+        await _planoService.DeletePlano(id);
+        return RedirectToAction(nameof(Index));
+    }   
+}
